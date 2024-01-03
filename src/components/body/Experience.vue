4a66c042e6518e3c37cfeaee7355298e6d672239
ceosalibio <salibioarceo@gmail.com>
2024-01-03 15:37:57 +0800

    initial experience

--- src/components/body/Experience.vue
+++ src/components/body/Experience.vue
@@ -1,19 +1,26 @@
 <template>
-    <div>
-        <h1>try</h1>
-    </div>
+  <div>
+        <v-row>
+            <v-col cols="12" md="6" lg="6" xl="6">
+                <v-card flat>
+                    <v-card-text>
+                        <h2>FULL STACK WEB DEVELOPER</h2>
+                        <h3>H.R.D Singapore Pte Ltd.</h3>
+                        <h4>2018 to Present</h4>
+
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+  </div>
 </template>
 
 <script>
 export default {
-    setup () {
-        
 
-        return {}
-    }
 }
 </script>
 
-<style  scoped>
+<style>
 
 </style>
